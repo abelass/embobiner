e157df7fe98393060c83540885ca7b6c76335cf1
Rainer Müller <rainer@websimple.be>
2014-04-06 10:13:53 +0200

    début du scrollable

---
+++ embobiner_pipelines.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Utilisations de pipelines par Citizen Art Days
+ *
+ * @plugin     Embobiner
+ * @copyright  2014
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Embobiner\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+?>
